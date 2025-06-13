570b8dda3db239fb9cde7495e093a35c44c8257e
ldlh615 <1207893654@qq.com>
2025-06-13 17:19:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# vite-plugin-move-html-resource
+
+## usage
+
+vite.config.js
+
+```
+import moveHtmlResourcePlugin from './move-html-resource-plugin';
+
+export default {
+    ...other config
+
+    plugins: [
+      moveHtmlResourcePlugin(),
+      ...other plugin
+    ],
+
+    ...other config
+}
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import path from 'path';
+
+export function transformHtml(subFilePath) {
+  let htmlContent = fs.readFileSync(subFilePath, 'utf-8');
+
+  let isInHead = false;
+  let isInBody = false;
+
+  const htmlContentSplits = htmlContent.split('\n');
+  const injectResources = [];
+
+  for (let i = 0; i < htmlContentSplits.length; i++) {
+    const line = htmlContentSplits[i];
+    if (line.includes('<head>')) {
+      isInHead = true;
+      continue;
+    }
+    if (line.includes('</head>')) {
+      isInHead = false;
+      continue;
+    }
+    if (line.includes('<body')) {
+      isInBody = true;
+      continue;
+    }
+    if (line.includes('</body>')) {
+      isInBody = false;
+      continue;
+    }
+    if (isInHead) {
+      if (/^\s+<(link|script)/.test(line) && /(rel|src|type)=\".*\"/.test(line)) {
+        injectResources.push(line);
+        htmlContentSplits.splice(i, 1);
+        i--;
+        continue;
+      }
+    }
+    if (isInBody) {
+      if (/<script/.test(line) && injectResources.length > 0) {
+        const injectCotnent = injectResources.join('\n');
+        htmlContentSplits.splice(i, 0, injectCotnent);
+        i += injectResources.length;
+        injectResources.length = 0;
+      }
+    }
+  }
+
+  const newHtmlContent = htmlContentSplits.join('\n');
+  fs.writeFileSync(subFilePath, newHtmlContent, 'utf-8');
+}
+
+export default function htmlTransfromPlugin() {
+  return {
+    name: 'vite-plugin-html-transform',
+
+    closeBundle(err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const distDir = path.resolve(process.cwd(), 'dist');
+      for (let subFile of fs.readdirSync(distDir)) {
+        const subFilePath = path.resolve(distDir, subFile);
+        const stat = fs.statSync(subFilePath);
+        if (stat.isFile() && path.extname(subFile) === '.html') {
+          transformHtml(subFilePath);
+        }
+      }
+      // console.log(bundle);
+    },
+  };
+}
